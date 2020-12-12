14e9aa6812bff60e81038162a47685667d9621be
Lightboom <55315954+Lightboom9@users.noreply.github.com>
2020-12-12 18:02:07 +0300

    !help added, minor updates

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace MATBOT
             _client.Log += Log;
             _client.MessageReceived += MessageLogger;
 
-            var token = File.ReadAllText(@"c:\outdir\botToken.txt");
+            string token = File.ReadAllText(@"c:\outdir\botToken.txt");
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
@@ -49,6 +49,8 @@ namespace MATBOT
             string codeToExecute = "";
 
             bool outputsText = true;
+            string prefix = "";
+            string postfix = "";
 
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
@@ -61,13 +63,30 @@ namespace MATBOT
                 {
                     EmbedBuilder builder = new EmbedBuilder();
                     
-
                     builder.Title = "This is a title.";
                     builder.AddField("This is a field.", "And *this* is a value.", true);
                     builder.ImageUrl = $"attachment://{_imageFilename}";
 
                     builder.WithColor(Color.Blue);
                     await msg.Channel.SendFileAsync(@"c:\outdir\" + _imageFilename, embed: builder.Build());
+
+                    return;
+                }
+                case "!help":
+                {
+                    EmbedBuilder builder = new EmbedBuilder();
+
+                    builder.Title = "List of commands";
+                    builder.Description = "Equations and sequences of elements in all other input elements must not contain spaces.\nInput elements in [] are mandatory. Input elements in () are optional.\nAdditional variables may represent any additional data.";
+                    builder.Color = Color.Blue;
+                    builder.Footer = new EmbedFooterBuilder().WithText("This bot uses MATLAB");
+
+                    builder.AddField("!help", "Provides the list of available commands.");
+                    builder.AddField("!solve", "Solves the given equation.\nSyntax: `!solve [equation] [unknown var] (additional vars)`\nExample: `!solve a*x+3+b==18 x a,b`");
+                    builder.AddField("!integrate", "Integrates the given equation.\nSyntax: `!integrate [function] [slice along var] (additional vars)`\nExample: `!integrate exp(x)*b+3*a-4 x a,b`");
+                    builder.AddField("!integrateDefinite", "Makes a definite integration of the given equation.\nSyntax: `!integrateDefinite [function] [slice along var] (additional vars) [bottom limit] [upper limit]`\nExample: `!integrateDefinite 2*x+3 x 1 4`");
+
+                    await msg.Channel.SendMessageAsync("", embed: builder.Build());
 
                     return;
                 }
@@ -80,13 +99,15 @@ namespace MATBOT
                         return;
                     }
 
+                    messages[1] = messages[1].Replace("=", "==");
+
                     if (messages.Length == 3)
                     {
-                        codeToExecute += "syms " + messages[2].Replace(',', ' ') + "; answerToThisDarkQuestion = solve(" + messages[1] + "," + messages[2] + "); answerString = evalc('pretty(answerToThisDarkQuestion)');";
+                        codeToExecute += "syms " + messages[2].Replace(',', ' ') + "; answerToThisDarkQuestion = solve(" + messages[1] + "," + messages[2] + "); answerString = evalc('answerToThisDarkQuestion');";
                     }
                     else
                     {
-                        codeToExecute += "syms " + messages[2].Replace(',', ' ') + " " + messages[3].Replace(',', ' ') + "; answerToThisDarkQuestion = solve(" + messages[1] + "," + messages[2] + "); answerString = evalc('pretty(answerToThisDarkQuestion)');";
+                        codeToExecute += "syms " + messages[2].Replace(',', ' ') + " " + messages[3].Replace(',', ' ') + "; answerToThisDarkQuestion = solve(" + messages[1] + "," + messages[2] + "); answerString = evalc('answerToThisDarkQuestion');";
                     }
 
                     break;
@@ -106,20 +127,60 @@ namespace MATBOT
                         return;
                     }
 
+                    messages[1] = messages[1].Replace("=", "==");
+
+                    prefix = $"f({messages[2]}) = ";
+                    postfix = " + C";
+
                     if (messages.Length == 3)
                     {
-                        codeToExecute += "syms " + messages[2] +$"; f = inline('{messages[1]}', '{messages[2]}'); answerToThisDarkQuestion = int(f({messages[2]}),'" + messages[2] + "'); answerString = evalc('pretty(answerToThisDarkQuestion)');";
+                        codeToExecute += "syms " + messages[2] +$"; f = inline('{messages[1]}', '{messages[2]}'); answerToThisDarkQuestion = int(f({messages[2]}),'" + messages[2] + "'); answerString = evalc('answerToThisDarkQuestion');";
                     }
                     else
                     {
                         string parms = "";
-                        for (int i = 0; i < messages[2].Length; i += 2)
+                        for (int i = 0; i < messages[3].Length; i += 2)
                         {
-                            parms += "'" + messages[2][i] + "'";
-                            if (i + 1 != messages[2].Length) parms += ",";
+                            parms += "'" + messages[3][i] + "'";
+                            if (i + 1 != messages[3].Length) parms += ",";
                         }
 
-                        codeToExecute += "syms " + messages[2].Replace(',', ' ') + " " + messages[3].Replace(',', ' ') + "; answerToThisDarkQuestion = solve(" + messages[1] + "," + messages[2] + "); answerString = evalc('pretty(answerToThisDarkQuestion)');";
+                        codeToExecute += "syms " + messages[2][0] + " " + messages[3].Replace(',', ' ') + $"; f = inline('{messages[1]}', '{messages[2]}', {parms}); answerToThisDarkQuestion = int(f({messages[2]}, {messages[3]}),'" + messages[2] + $"'); answerString = evalc('answerToThisDarkQuestion');";
+                    }
+
+                    break;
+                }
+                case "!integrateDefinite":
+                {
+                    if (messages.Length < 5 || messages.Length > 6)
+                    {
+                        await msg.Channel.SendMessageAsync("Wrong input.");
+
+                        return;
+                    }
+                    if (messages[2].Length > 1)
+                    {
+                        await msg.Channel.SendMessageAsync("Wrong input.");
+
+                        return;
+                    }
+
+                    messages[1] = messages[1].Replace("=", "==");
+
+                    if (messages.Length == 5)
+                    {
+                        codeToExecute += "syms " + messages[2] + $"; f = inline('{messages[1]}', '{messages[2]}'); answerToThisDarkQuestion = int(f({messages[2]}),'" + messages[2] + $"', {messages[3]}, {messages[4]}); answerString = evalc('answerToThisDarkQuestion');";
+                    }
+                    else
+                    {
+                        string parms = "";
+                        for (int i = 0; i < messages[3].Length; i += 2)
+                        {
+                            parms += "'" + messages[3][i] + "'";
+                            if (i + 1 != messages[3].Length) parms += ",";
+                        }
+
+                        codeToExecute += "syms " + messages[2][0] + " " + messages[3].Replace(',', ' ') + $"; f = inline('{messages[1]}', '{messages[2]}', {parms}); answerToThisDarkQuestion = int(f({messages[2]}, {messages[3]}),'" + messages[2] + $"', {messages[3]}, {messages[4]}); answerString = evalc('answerToThisDarkQuestion');";
                     }
 
                     break;
@@ -140,20 +201,37 @@ namespace MATBOT
             {
                 process.Start();
 
-                await Task.Delay(5000);
-
-                FileInfo info = new FileInfo(_outputTextFilename);
-                while (IsFileInUse(info))
-                {
-                    await Task.Delay(1000);
-                }
-
-                await Task.Delay(1000);
-
                 EmbedBuilder builder = new EmbedBuilder();
+                builder.Title = "Output";
+                builder.WithColor(Color.Blue);
 
-                string answer = File.ReadAllText(_outputTextFilename);
-                await msg.Channel.SendMessageAsync("Output:\n" + answer);
+                if (outputsText)
+                {
+                    await Task.Delay(5000);
+
+                    FileInfo info = new FileInfo(_outputTextFilename);
+                    while (IsFileInUse(info))
+                    {
+                        await Task.Delay(1000);
+                    }
+
+                    await Task.Delay(1000);
+
+                    string answer = File.ReadAllText(_outputTextFilename);
+                    answer = answer.Replace("*", "\\*");
+                    answer = answer.Replace("answerToThisDarkQuestion =", "");
+                    answer.Replace("Empty sym: 0-by-1", "Answer can't be found or there's an error in the input");
+                    answer = answer.Trim();
+                    answer = prefix + answer + postfix;
+
+                    builder.WithDescription(answer);
+
+                    await msg.Channel.SendMessageAsync("", embed: builder.Build());
+                }
+                else
+                {
+
+                }
 
                 process.Kill();
             }
